4fc2eaa117861b6513b00c9dab94cfbb65afaafd
Tristan <t-heraud@outlook.fr>
2024-06-06 23:04:11 +0200

    Description correcte

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -44,7 +44,7 @@ class Weather {
     return Weather(
       cityName: city,
       temperature: (json['current_weather']['temperature'] ?? 0.0) as double,
-      description: (json['current_weather']['weathercode'] ?? 'Ensoleillé').toString(), // Modifier selon les données réelles
+      description: _mapWeatherCodeToDescription(json['current_weather']['weathercode']),
       windSpeed: (json['current_weather']['windspeed'] ?? 0.0) as double,
       humidity: (json['current_weather']['humidity'] ?? 0) as int,
       feelsLike: (json['current_weather']['temperature'] ?? 0.0) as double, // Remplacer par les données réelles si disponibles
@@ -53,6 +53,42 @@ class Weather {
       dailyForecasts: dailyForecasts,
       chanceOfRain: (json['daily']['precipitation_probability_mean'][0] ?? 0) as int,
     );
+  }
+
+  static String _mapWeatherCodeToDescription(int code) {
+    switch (code) {
+      case 0:
+        return 'Ensoleillé';
+      case 1:
+        return 'Principalement ensoleillé';
+      case 2:
+        return 'Partiellement nuageux';
+      case 3:
+        return 'Nuageux';
+      case 45:
+      case 48:
+        return 'Brouillard';
+      case 51:
+      case 53:
+      case 55:
+        return 'Bruine légère';
+      case 61:
+      case 63:
+      case 65:
+        return 'Pluie';
+      case 71:
+      case 73:
+      case 75:
+        return 'Neige';
+      case 80:
+      case 81:
+      case 82:
+        return 'Averses';
+      case 95:
+        return 'Orages';
+      default:
+        return 'Non disponible';
+    }
   }
 }
 
